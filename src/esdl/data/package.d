7b8d98c1c85a999913f1925f90eb7cf60e82dcbf
Puneet Goel <puneet@coverify.com>
2022-07-13 22:11:19 +0530

    Added packed construct for bitfield randomization

--- src/esdl/data/package.d
+++ src/esdl/data/package.d
@@ -3,6 +3,7 @@ module esdl.data;
 public import esdl.data.bstr;
 public import esdl.data.bvec;
 public import esdl.data.deck;
+public import esdl.data.packed;
 public import esdl.data.packer;
 public import esdl.data.queue;
 public import esdl.data.sync;
