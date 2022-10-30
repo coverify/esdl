d0ca62e474088274c56f01ce8b2516e48c9d3fd3
Puneet Goel <puneet@coverify.com>
2022-10-30 16:13:29 +0530

    Added new implementation of Charbuf

--- src/esdl/data/package.d
+++ src/esdl/data/package.d
@@ -2,6 +2,7 @@ module esdl.data;
 
 public import esdl.data.bstr;
 public import esdl.data.bvec;
+public import esdl.data.charbuf;
 public import esdl.data.packed;
 public import esdl.data.packer;
 public import esdl.data.queue;
